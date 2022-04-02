560363eb02ed5272656d6d76e091989ecb601b5e
Neil Jasani <jasanineil@gmail.com>
2022-04-02 00:25:42 -0500

    Implement ability to refresh, see last 20 posts, and switch tabs.

---
+++ app/src/main/java/com/example/parstagram/fragments/FeedFragment.kt
@@ -0,0 +1,95 @@
+package com.example.parstagram.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.parstagram.MainActivity
+import com.example.parstagram.Post
+import com.example.parstagram.PostAdapter
+import com.example.parstagram.R
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
+
+open class FeedFragment : Fragment() {
+
+    lateinit var rvPosts: RecyclerView
+    lateinit var adapter: PostAdapter
+    lateinit var swipeContainer: SwipeRefreshLayout
+
+    var allPosts: MutableList<Post> = mutableListOf()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set up views and click listeners
+        rvPosts = view.findViewById(R.id.rvPosts)
+        swipeContainer = view.findViewById(R.id.swipeContainer)
+        swipeContainer.setOnRefreshListener {
+            queryPosts()
+        }
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light)
+
+        adapter = PostAdapter(requireContext(), allPosts)
+        rvPosts.adapter = adapter
+        rvPosts.layoutManager = LinearLayoutManager(requireContext())
+
+        queryPosts()
+    }
+
+    // Query for all posts in our server
+    open fun queryPosts() {
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        // Find all Post objects
+        query.include(Post.KEY_USER)
+        // Return posts in descending order (newest posts first)
+        query.addDescendingOrder("createdAt")
+        // Return the 20 most recent posts. POST_LIMIT defined in companion object
+        query.setLimit(POST_LIMIT)
+
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e != null) {
+                    // Something went wrong
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i(TAG, "Post: " + post.getDescription() + " , username: " +
+                                    post.getUser()?.username)
+                        }
+                        adapter.clear()
+                        adapter.addAll(posts)
+                        // Signal that the refresh has finished
+                        swipeContainer.setRefreshing(false)
+                    }
+                }
+            }
+        })
+    }
+
+    companion object {
+        const val TAG = "FeedFragment"
+        const val POST_LIMIT = 20
+    }
+}
